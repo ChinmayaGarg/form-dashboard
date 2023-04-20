765c48e2cbce62fbf209d2a0db01d73a04856a8f
ChinmayaGarg <chinmaya.garg1997@gmail.com>
2023-04-20 20:44:23 +0530

    Code Cleaned

--- src/pages/forms/Form.jsx
+++ src/pages/forms/Form.jsx
@@ -1,16 +1,15 @@
-import "./single.scss";
-import Sidebar from "../../components/sidebar/Sidebar";
-import Navbar from "../../components/navbar/Navbar";
-import Chart from "../../components/chart/Chart";
-import List from "../../components/table/Table";
+import Navbar from '../../components/navbar/Navbar';
+import Sidebar from '../../components/sidebar/Sidebar';
+import './form.scss';
 
-const Single = () => {
+const Form = () => {
   return (
     <div className="single">
       <Sidebar />
       <div className="singleContainer">
         <Navbar />
-        <div className="top">
+
+        {/* <div className="top">
           <div className="left">
             <div className="editButton">Edit</div>
             <h1 className="title">Information</h1>
@@ -32,9 +31,7 @@ const Single = () => {
                 </div>
                 <div className="detailItem">
                   <span className="itemKey">Address:</span>
-                  <span className="itemValue">
-                    Elton St. 234 Garden Yd. NewYork
-                  </span>
+                  <span className="itemValue">Elton St. 234 Garden Yd. NewYork</span>
                 </div>
                 <div className="detailItem">
                   <span className="itemKey">Country:</span>
@@ -46,14 +43,14 @@ const Single = () => {
           <div className="right">
             <Chart aspect={3 / 1} title="User Spending ( Last 6 Months)" />
           </div>
-        </div>
-        <div className="bottom">
-        <h1 className="title">Last Transactions</h1>
-          <List/>
-        </div>
+        </div> */}
+        {/* <div className="bottom">
+          <h1 className="title">Last Transactions</h1>
+          <List />
+        </div> */}
       </div>
     </div>
   );
 };
 
-export default Single;
+export default Form;
